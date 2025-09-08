602d75851a2562f462f5a2468e437485e35737b0
Matt <git@crooks.com.au>
2025-09-08 17:02:07 +0800

    feat: add macro88 public key to SimpleRepository initialization

--- whitelist-plugin/pkg/repository/simple_repository.go
+++ whitelist-plugin/pkg/repository/simple_repository.go
@@ -11,6 +11,7 @@ func NewSimpleRepository() *SimpleRepository {
 		"e56c3d2fd8e0309264e558309ae65e31328fe0a2e1fe26cf4074a8ff1df48fc2", //eventforwarder
 		"d91191e30e00444b942c0e82cad470b32af171764c2275bee0bd99377efd4075", //gsov
 		"a0dda882fb89732b04793a2c989435fcd89ee559e81291074450edbd9b15621b", //rocketdog8
+		"ba1838441e720ee91360d38321a19cbf8596e6540cfa045c9c5d429f1a2b9e3a", //macro88
 	}
 
 	keys := make([][32]byte, 0, len(pubKeys))
